97737b01400111b1789229b7821bde636f000aef
maciek97x <maciek97x@o2.pl>
2020-05-31 21:51:22 +0200

    Added water bolts.

---
+++ Assets/Scripts/WaterBoltDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBoltDamage : MonoBehaviour
+{
+    public float damage = 1.0f;
+    public float startForce = 2.0f;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(transform.forward * startForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(rigidbody.velocity, transform.up);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //Destroy(gameObject);
+    }
+}

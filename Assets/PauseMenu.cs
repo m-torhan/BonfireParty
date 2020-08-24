85707bc099e9b4cd5bfd7714d2592c5d39f4c2d1
katwojewoda <44870269+katwojewoda@users.noreply.github.com>
2020-08-24 11:15:58 +0200

    Menu&NavMesh
    Menu, Pause Menu (on P), Nav mesh addes

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePause = false;
+    public GameObject pauseMenuUI;
+        void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(GamePause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+
+        }
+       
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}

9ee0507f02f0a2923acf0472778b6232c3490154
maciek97x <maciek97x@o2.pl>
2020-08-28 15:33:57 +0200

    Improved weapon handling

---
+++ Assets/Scripts/WeaponProperties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class WeaponProperties
+{
+    public float drawTime { private set; get; }
+    public float reloadTime { private set; get; }
+    public float rateOfFire { private set; get; }
+    public float damageAvg { private set; get; }
+    public float damageStdDev { private set; get; }
+    public float ammoUsage { private set; get; }
+    public float magazineSize { private set; get; }
+
+    public WeaponProperties(float drawTime, float reloadTime, float rateOfFire, float damageAvg, float damageStdDev, float ammoUsage, float magazineSize)
+    {
+        this.drawTime = drawTime;
+        this.reloadTime = reloadTime;
+        this.rateOfFire = rateOfFire;
+        this.damageAvg = damageAvg;
+        this.damageStdDev = damageStdDev;
+        this.ammoUsage = ammoUsage;
+        this.magazineSize = magazineSize;
+    }
+
+}
---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public WeaponProperties weaponProperties { private set; get; } = new WeaponProperties(2.0f, 1.0f, 0.5f, 5.0f, 1.0f, 2.0f, 10.0f);
+
+    public GameObject barrelEnd;
+
+    private float reloadTime = 0f;
+    private float drawTime = 0f;
+    private float shotTime = 0f;
+    private float magazineAmmo = 0f;
+    private float ammo = 0f;
+
+    [SerializeField]
+    private GameObject waterBoltPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shotTime >= 0)
+        {
+            shotTime -= Time.deltaTime;
+        }
+
+        if (drawTime >= 0)
+        {
+            drawTime -= Time.deltaTime;
+        }
+
+        if (reloadTime >= 0)
+        {
+            reloadTime -= Time.deltaTime;
+            if (reloadTime < 0)
+            {
+                magazineAmmo = ammo >= weaponProperties.magazineSize ? weaponProperties.magazineSize : ammo;
+            }
+        }
+    }
+
+    public void Shot()
+    {
+        if (shotTime < 0 && reloadTime < 0 && drawTime < 0 && magazineAmmo > 0 && barrelEnd != null)
+        {
+            Instantiate(waterBoltPrefab, barrelEnd.transform.position, barrelEnd.transform.rotation);
+            shotTime = weaponProperties.rateOfFire;
+            magazineAmmo -= weaponProperties.ammoUsage;
+        }
+    }
+
+    public void Reload()
+    {
+        if (reloadTime < 0 && barrelEnd != null)
+        {
+            reloadTime = weaponProperties.reloadTime;
+        }
+    }
+
+    public void Draw()
+    {
+        drawTime = weaponProperties.drawTime;
+    }
+}

92cdaf6cf34466b4a4992ffccf0a12aea1755556
jacekpotapczuk <jacekpotapczuk@gmail.com>
2020-09-01 02:30:17 +0200

    update PauseMenu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -45,13 +45,11 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        
+        GameManager.Instance.LoadMainMenu();
     }
 
     public void QuitGame ()
     {
-        Debug.Log("QUIT!");
         Application.Quit();
     }
 
